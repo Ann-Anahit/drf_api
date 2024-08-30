73eb60c5c3c948fe1be32b9290d01e454b619acd
Anna Afijan <ann.afijan@gmail.com>
2024-08-30 09:00:40 +0000

    change views py drf api

--- drf_api/views.py
+++ drf_api/views.py
@@ -1,9 +1,8 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
-
-@api_view()
+@api_view(['GET'])
+@permission_classes([AllowAny])
 def root_route(request):
-    return Response({
-        "message": "Welcome to my drf API!"
-    })
+    return Response({"message": "Welcome to the API!"})
