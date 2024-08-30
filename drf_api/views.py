642bcd404241f2163f6fe6c4c519eb7b01dacc61
Anna Afijan <ann.afijan@gmail.com>
2024-08-30 09:21:35 +0000

    add jwt to views

--- drf_api/views.py
+++ drf_api/views.py
@@ -1,6 +1,9 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from .settings import (
+    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE, JWT_AUTH_SECURE,
+)
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
