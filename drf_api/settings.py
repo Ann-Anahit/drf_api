58b14b23d6767d258dfe4be9afea61894f8847c0
Anna Afijan <ann.afijan@gmail.com>
2024-09-05 08:09:52 +0000

    add url

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -116,12 +116,14 @@ if 'CLIENT_ORIGIN' in os.environ:
    CORS_ALLOWED_ORIGINS = [
     os.environ.get('CLIENT_ORIGIN'),
     'https://meetandmingle-01eaa763465e.herokuapp.com',
-    'https://drf-api-pp-0ae57f00f3cd.herokuapp.com',  
+    'https://drf-api-pp-0ae57f00f3cd.herokuapp.com', 
 ]
 
 if 'CLIENT_ORIGIN_DEV' in os.environ:
     CORS_ALLOWED_ORIGIN_REGEXES = [r"^https://.*\.codeinstitute-ide\.net$",
                                     r"^https://.*\.herokuapp\.com$",
+                                    "https://8000-annanahit-p5advancedfro-fl0rpyae2is.ws.codeinstitute-ide.net",
+
     ]
 
 CORS_ALLOW_CREDENTIALS = True
