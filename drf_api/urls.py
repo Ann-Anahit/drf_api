93af3b48e515d7545258ee27a91473fad5a466f4
Anna Afijan <ann.afijan@gmail.com>
2024-09-11 20:58:29 +0000

    update urls

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -3,18 +3,21 @@ from django.urls import path, include
 from .views import root_route, logout_route
 
 urlpatterns = [
-    path('', root_route),  # Root route
+    path('', root_route),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),  # DRF authentication URLs
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),  # DJ REST Auth URLs
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),  # DJ REST Auth Registration URLs
-    path('dj-rest-auth/logout/', logout_route),  # Custom logout route
-
-    # Include app-specific URLs
-    path('profiles/', include('profiles.urls')),
-    path('posts/', include('posts.urls')),
-    path('comments/', include('comments.urls')),
-    path('likes/', include('likes.urls')),
-    path('followers/', include('followers.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('dj-rest-auth/logout/', logout_route),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('', include('profiles.urls')),
+    path('', include('posts.urls')),
+    path('', include('comments.urls')),
+    path('', include('likes.urls')),
+    path('', include('followers.urls')),
     path('messages/', include('messaging.urls')),
 ]
+
+
+
+
+    
