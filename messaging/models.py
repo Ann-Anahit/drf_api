d1c8fd96004cb859024e0b8bb24d5350b9498629
Anna Afijan <ann.afijan@gmail.com>
2024-11-06 17:13:03 +0000

    change message app

--- messaging/models.py
+++ messaging/models.py
@@ -1,18 +1,21 @@
+# models.py
+from django.contrib.auth import get_user_model
 from django.db import models
-from django.contrib.auth.models import User
 
-class Message(models.Model):
-    """
-    Message model, related to User
-    """
-    sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
-    receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
+User = get_user_model()
+
+class Conversation(models.Model):
+    participants = models.ManyToManyField(User)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    content = models.TextField()
-
-    class Meta:
-        ordering = ['-created_at']
 
     def __str__(self):
-        return f"From {self.sender.username} to {self.receiver.username}: {self.content[:20]}"
+        return f"Conversation between {', '.join(participant.username for participant in self.participants.all())}"
+
+class Message(models.Model):
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.sender.username} at {self.created_at}"
