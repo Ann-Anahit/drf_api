8b3ffdf8bea43054bd70170f14557f0fe144fdd2
Anna Afijan <ann.afijan@gmail.com>
2024-11-10 23:06:30 +0000

    change urls

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -1,28 +1,35 @@
-from django.contrib import admin
-from django.urls import path, include
-from .views import root_route, logout_route
-from rest_framework.authtoken.views import obtain_auth_token
+"""drf_api URL Configuration
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path, include
 from .views import root_route, logout_route
-from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('', root_route),  # Root route
-    path('admin/', admin.site.urls),  # Django admin interface
-
-    # Authentication URLs
-    path('api-auth/', include('rest_framework.urls')),  # DRF auth
-    path('dj-rest-auth/logout/', logout_route),  # Custom logout route
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),  # dj-rest-auth for login, password reset, etc.
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),  # dj-rest-auth registration
-
-    # App-specific URLs
-    path('profiles/', include('profiles.urls')),  # Profiles app
-    path('posts/', include('posts.urls')),  # Posts app
-    path('comments/', include('comments.urls')),  # Comments app
-    path('likes/', include('likes.urls')),  # Likes app
-    path('followers/', include('followers.urls')),  # Followers app
-    path('groups/', include('groups.urls')),  # Groups app
+    path('', root_route),
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('dj-rest-auth/logout/', logout_route),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path(
+        'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
+    ),
+    path('', include('profiles.urls')),
+    path('', include('posts.urls')),
+    path('', include('comments.urls')),
+    path('', include('likes.urls')),
+    path('', include('followers.urls')),
+    path('', include('groups.urls')),
 ]
