661557beeb58959b27ecb0e048f82f70508be75f
Anna Afijan <ann.afijan@gmail.com>
2024-11-28 17:06:33 +0000

    changes

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -239,5 +239,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-print('DEBUG: ', DEBUG)
