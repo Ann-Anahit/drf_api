4b63fb8efad373fe6bf47f96be2765c21ec4eb0f
Anna Afijan <ann.afijan@gmail.com>
2025-01-02 16:14:21 +0000

    change client origin settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -123,7 +123,8 @@ MIDDLEWARE = [
 #     ]
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
-os.environ.get('CLIENT_ORIGIN'),
+        os.environ.get('CLIENT_ORIGIN'),
+        os.environ.get('CLIENT_ORIGIN_DEV')
 ]
 else:
     CORS_ALLOWED_ORIGIN_REGEXES = [r"^https://.*\.codeinstitute-ide\.net$",]
