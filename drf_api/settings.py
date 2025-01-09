731dbb6c79da88991e960ca436a4f44c2b2a7cc6
Anna Afijan <ann.afijan@gmail.com>
2025-01-09 19:01:28 +0000

    change settings , add client origin to env py

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -118,12 +118,19 @@ MIDDLEWARE = [
 ]
 
 
+# if 'CLIENT_ORIGIN' in os.environ:
+#     CORS_ALLOWED_ORIGINS = [
+#         os.environ.get('CLIENT_ORIGIN'),
+#         os.environ.get('CLIENT_ORIGIN_DEV')
+# ]
+# else:
+#     CORS_ALLOWED_ORIGIN_REGEXES = [r"^https://.*\.codeinstitute-ide\.net$",]
+
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN'),
-        os.environ.get('CLIENT_ORIGIN_DEV')
-]
-else:
+        os.environ.get('CLIENT_ORIGIN')
+    ]
+if 'CLIENT_ORIGIN_DEV' in os.environ:    
     CORS_ALLOWED_ORIGIN_REGEXES = [r"^https://.*\.codeinstitute-ide\.net$",]
                                         
 
