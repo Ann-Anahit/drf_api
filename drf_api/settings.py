5dab125a97458310e923ff87965e25ce46c76830
Anna Afijan <ann.afijan@gmail.com>
2025-01-09 19:03:03 +0000

    remove comented out code from settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -117,15 +117,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-
-# if 'CLIENT_ORIGIN' in os.environ:
-#     CORS_ALLOWED_ORIGINS = [
-#         os.environ.get('CLIENT_ORIGIN'),
-#         os.environ.get('CLIENT_ORIGIN_DEV')
-# ]
-# else:
-#     CORS_ALLOWED_ORIGIN_REGEXES = [r"^https://.*\.codeinstitute-ide\.net$",]
-
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
