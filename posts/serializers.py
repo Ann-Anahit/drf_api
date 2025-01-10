441fbda0fe924cc849f8f67fd61c3b6cdb07c100
Anna Afijan <ann.afijan@gmail.com>
2025-01-10 20:50:55 +0000

    add category to serializers

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from posts.models import Post
 from likes.models import Like
+from category.models import PostCategory
+
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
@@ -10,9 +12,10 @@ class PostSerializer(serializers.ModelSerializer):
     like_id = serializers.SerializerMethodField()
     likes_count = serializers.ReadOnlyField()
     comments_count = serializers.ReadOnlyField()
-    category_id = serializers.ReadOnlyField()
+    category = serializers.PrimaryKeyRelatedField(
+        queryset=PostCategory.objects.all()
+    )
     category_name = serializers.ReadOnlyField(source='category.name')
-  
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
@@ -47,5 +50,5 @@ class PostSerializer(serializers.ModelSerializer):
             'profile_image', 'created_at', 'updated_at',
             'title', 'content', 'image', 'image_filter',
             'like_id', 'likes_count', 'comments_count',
-            'category_id', 'category_name'
+            'category', 'category_name',
         ]
