95870b1c63ded3cbc049cef1d06c7c422ddd1d95
Anna Afijan <ann.afijan@gmail.com>
2025-01-11 16:39:53 +0000

    change admin py in category

--- category/admin.py
+++ category/admin.py
@@ -3,10 +3,5 @@ from .models import PostCategory
 
 @admin.register(PostCategory)
 class PostCategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'image_thumbnail') 
+     list_display = ('name', 'image')
 
-    def image_thumbnail(self, obj):
-        if obj.image:
-            return f'<img src="{obj.image.url}>'
-        return 'No Image'
-    image_thumbnail.allow_tags = True 
