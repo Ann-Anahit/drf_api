5eb883fbb8a4ef5b7667942095be9194313a18a7
Anna Afijan <ann.afijan@gmail.com>
2025-01-11 22:13:03 +0000

    add update image to category model

--- category/models.py
+++ category/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class PostCategory(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    image = models.ImageField(blank=True)
+    image = models.ImageField(upload_to='images/', blank=True)
+
 
     def __str__(self):
         return self.name
