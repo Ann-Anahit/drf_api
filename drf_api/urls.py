2b33aa64bb050609323958cbe3b72bceaa41c951
Anna Afijan <ann.afijan@gmail.com>
2025-01-15 20:39:14 +0000

    add event to drf api urls

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -36,5 +36,6 @@ urlpatterns = [
     path('', include('comments.urls')),
     path('', include('likes.urls')),
     path('', include('followers.urls')),
+    path('', include('event.urls')),
     path('', include(router.urls)),
 ]
