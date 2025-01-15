98c3de1a81aa4d6eb59ec395657e118832acb7e0
Anna Afijan <ann.afijan@gmail.com>
2025-01-15 20:45:20 +0000

    add slash to event urls.py

--- event/urls.py
+++ event/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from event import views
 
 urlpatterns = [
-    path('event', views.EventList.as_view()),
+    path('event/', views.EventList.as_view()),
     path('event/<int:pk>/', views.EventDetail.as_view())
 ]
