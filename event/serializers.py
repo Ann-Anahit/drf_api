97594585d271febb0cd946fc7fe05a17aa83deb8
Anna Afijan <ann.afijan@gmail.com>
2025-01-15 00:09:01 +0000

    add serializers and views to event

---
+++ event/serializers.py
@@ -0,0 +1,42 @@
+from rest_framework import serializers
+from django.utils import timezone
+from events.models import Event
+
+class EventSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    event_date = serializers.DateTimeField(format='%d %b %Y %H:%M')
+
+    def validate_image(self, value):
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError('Image size larger than 2MB!')
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px!'
+            )
+        return value
+
+    def validate_event_date(self, value):
+        if value < timezone.now():
+            raise serializers.ValidationError(
+                'The event date cannot be in the past.'
+            )
+        return value
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Event
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id',
+            'profile_image', 'created_at', 'updated_at',
+            'title', 'description', 'event_date', 'event_image',
+        ]
--- event/views.py
+++ event/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from connectify_api.permissions import IsOwnerOrReadOnly
+from .models import Event
+from .serializers import EventSerializer
 
-# Create your views here.
+
+class EventList(generics.ListCreateAPIView):
+    serializer_class = EventSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Event.objects.all().order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'favorites__owner__profile',
+        'owner__profile',
+        'event_date',
+    ]
+    search_fields = [
+        'owner__username',
+        'title',
+        'event_date',
+    ]
+    ordering_fields = []
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class EventDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = EventSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Event.objects.all().order_by('-created_at')
