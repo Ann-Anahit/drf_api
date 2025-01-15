878ea88f2052e7e50cd53dfc4b150841c2083136
Anna Afijan <ann.afijan@gmail.com>
2025-01-15 12:04:57 +0000

    add field duration to event and migrate the model

--- event/models.py
+++ event/models.py
@@ -12,11 +12,10 @@ class Event(models.Model):
     description = models.TextField(blank=True)
     location = models.CharField(max_length=255)
     event_start = models.DateTimeField()
-    duration = models.CharField(max_length=100)
+    duration = models.CharField(max_length=100, default='0 hours')
     event_image = models.ImageField(
         upload_to='images/', default='../default_xgered.webp', blank=True
     )
-  
 
     class Meta:
         ordering = ['-created_at']
@@ -26,3 +25,21 @@ class Event(models.Model):
 
     def clean(self):
         super().clean()
+
+        duration_parts = self.duration.split()
+        if len(duration_parts) != 2:
+            raise ValidationError("Duration must be in the format '<value> <unit>' (e.g., '1 hour', '2 days').")
+
+        try:
+            value = int(duration_parts[0])
+        except ValueError:
+            raise ValidationError("Duration value must be an integer.")
+        
+        unit = duration_parts[1].lower()
+        valid_units = ['hour', 'day', 'month']
+        if unit not in valid_units:
+            raise ValidationError(f"Duration unit must be one of {', '.join(valid_units)}.")
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
