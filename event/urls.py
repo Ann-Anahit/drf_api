c011e4846387926981ef84b7d639e0be14274c84
Anna Afijan <ann.afijan@gmail.com>
2025-01-19 18:46:11 +0000

    Add support for my events

--- event/urls.py
+++ event/urls.py
@@ -3,5 +3,6 @@ from event import views
 
 urlpatterns = [
     path('event/', views.EventList.as_view()),
+    path('myevent/', views.MyEventList.as_view()),
     path('event/<int:pk>/', views.EventDetail.as_view())
 ]
--- event/views.py
+++ event/views.py
@@ -30,6 +30,35 @@ class EventList(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
+class MyEventList(generics.ListCreateAPIView):
+    serializer_class = EventSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Event.objects.all().order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'owner__profile',
+        'event_start',
+    ]
+    search_fields = [
+        'owner__username',
+        'title',
+        'event_start',
+    ]
+    ordering_fields = []
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return Event.objects.filter(owner=self.request.user).order_by('-created_at')
+        return Event.objects.none()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 
 class EventDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = EventSerializer
